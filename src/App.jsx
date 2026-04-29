450d88e37db5c1d89c13a1ea68ae02374b745b31
satyammishra6745 <151515400+satyammishra6745@users.noreply.github.com>
2026-04-29 18:36:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Gallery from "./Pages/Gallery";
 import Services from "./Pages/Services";
 import About from "./Pages/About"
 import Contact from "./Pages/Contact"
-// const Contact = () => <h1 className="text-center mt-5">Contact Page</h1>;
+
 
 function App() {
 
